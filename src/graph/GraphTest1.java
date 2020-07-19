6781b71d8b188ac6d5c982042417781835285ea7
zengyujie <smalljingru@126.com>
2020-07-19 23:40:46 +0800

    finish course

--- src/graph/GraphTest1.java
+++ src/graph/GraphTest1.java
@@ -3,6 +3,8 @@ package graph;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.TreeSet;
 
 public class GraphTest1 {
 	
@@ -15,24 +17,27 @@ public class GraphTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] vertexValues = {"1","2","3","4","5","6","7","8"};
+		String[] vertexValues = {"A","B","C","D","E","F","G"};
 		Graph g = new Graph(vertexValues.length);
 		for(String s: vertexValues){
 			g.insertVertex(s);
 		}
-		g.insertEdge(0,1,1);
-		g.insertEdge(0,2,1);
-		g.insertEdge(1,3,1);
-		g.insertEdge(1,4,1);
-		g.insertEdge(2,5,1);
-		g.insertEdge(2,6,1);
-		g.insertEdge(3,7,1);
-		g.insertEdge(4,7,1);
-		g.showGraph();
-		g.DFS(0);
-		System.out.println("-----------");
-		g.BFS(0);
+		g.insertEdge(0,1,5);
+		g.insertEdge(0,2,7);
+		g.insertEdge(0,6,2);
+		g.insertEdge(1,3,9);
+		g.insertEdge(1,6,3);
+		g.insertEdge(2,4,8);
+		g.insertEdge(3,5,4);
+		g.insertEdge(4,5,5);
+		g.insertEdge(4,6,4);
+		g.insertEdge(5,6,6);
 		
+		g.showGraph();
+
+		System.out.println("-----------");
+
+		g.floyd();
 	}
 	
 	
@@ -42,12 +47,17 @@ public class GraphTest1 {
 class Graph{
 	
 	private ArrayList<String> vertexList;
+	private ArrayList<int[]> edgeSet;
 	private int[][] edges;
 	private int numOfEdges;
 	
 	public Graph(int n){
 		edges = new int[n][n];
+		for(int i = 0; i < n; i++){
+			Arrays.fill(edges[i], Integer.MAX_VALUE);
+		}
 		vertexList = new ArrayList<>(n);
+		edgeSet = new ArrayList<>();
 		numOfEdges = 0;
 	}
 	
@@ -58,6 +68,11 @@ class Graph{
 	public void insertEdge(int v1, int v2, int weight){
 		edges[v1][v2] = weight;
 		edges[v2][v1] = weight;
+		int e[] = new int[3];
+		e[0] = v1;
+		e[1] = v2;
+		e[2] = weight;
+		edgeSet.add(e);
 		numOfEdges++;
 	}
 	
@@ -73,6 +88,15 @@ class Graph{
 		return this.vertexList.get(i);
 	}
 	
+	public int getIndexByValue(String str){
+		for(int i = 0; i < this.vertexList.size(); i++){
+			if(this.vertexList.get(i).equals(str)){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
 	public int getWeight(int v1, int v2){
 		return edges[v1][v2];
 	}
@@ -80,6 +104,18 @@ class Graph{
 	public void showGraph(){
 		for(int i = 0; i < edges.length; i++){
 			System.out.println(Arrays.toString(edges[i]));
+		}
+	}
+	
+	public <T> void showGraph(T arr[][]){
+		for(int i = 0; i < edges.length; i++){
+			System.out.println(Arrays.toString(arr[i]));
+		}
+	}
+	
+	public void showGraph(int arr[][]){
+		for(int i = 0; i < edges.length; i++){
+			System.out.println(Arrays.toString(arr[i]));
 		}
 	}
 	
@@ -119,5 +155,166 @@ class Graph{
 			}
 		}
 	}
+	
+	public void prim(int n){
+		int vertexs[] = new int[this.vertexList.size()];
+		int paths[] = new int[this.vertexList.size()];
+		Arrays.fill(paths, Integer.MAX_VALUE);
+		paths[n] = 0;
+		vertexs[n] = n;
+		ArrayList<String> list = new ArrayList<>();
+		list.add(this.getValueByIndex(n));
+		while(list.size() < vertexs.length){
+			int min = Integer.MAX_VALUE;
+			for(int i = 0; i < list.size(); i++){
+				int v = this.getIndexByValue(list.get(i));
+				for(int j = 0; j < vertexs.length; j++){
+					if(!list.contains(this.getValueByIndex(j)) && edges[v][j] != Integer.MAX_VALUE){
+						int t1 = paths[v] + edges[v][j];
+						if(t1 < paths[j]){
+							paths[j] = t1;
+							vertexs[j] = v;
+						}
+						if(edges[v][j] < min){
+							min = edges[v][j];
+							n = j;
+							vertexs[j] = v;
+						}
+					}
+				}
+			}
+			list.add(this.getValueByIndex(n));	
+		}
+		System.out.println(Arrays.toString(paths));
+		System.out.println(Arrays.toString(vertexs));
+	}
+	
+	
+	public void krus(){
+		Collections.sort(this.edgeSet, (e1, e2)->{
+			return Integer.compare(e1[2], e2[2]);
+		});
+		ArrayList<int[]> lists = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> conn = new ArrayList<>();
+		for(int i = 0; i < this.vertexList.size(); i++){
+			ArrayList<Integer> li = new ArrayList<>();
+			li.add(this.getIndexByValue(this.vertexList.get(i)));
+			conn.add(li);
+		}
+		while(!this.edgeSet.isEmpty()&& conn.size() > 1){
+			int[] temp = this.edgeSet.remove(0);
+			int a1 = 0;
+			int a2 = 0;
+			for(int i = 0; i < conn.size(); i++){
+				if(conn.get(i).contains(temp[0])){
+					a1 = i;
+				}
+				if(conn.get(i).contains(temp[1])){
+					a2 = i;
+				}
+			}
+			if(a1 != a2){
+				lists.add(temp);
+				conn.get(a1).addAll(conn.get(a2));
+				conn.remove(a2);
+			}
+		}
+		lists.forEach((e)->{
+			System.out.println(Arrays.toString(e));
+		});
+	}
+	
+	public void djistra(int n){
+		int[] pre = new int[this.vertexList.size()];
+		Arrays.fill(pre, -1);
+		int[] path = new int[pre.length];
+		Arrays.fill(path, Integer.MAX_VALUE);
+		ArrayList<Integer> list = new ArrayList<>();
+		pre[n] = n;
+		path[n] = 0;
+		while(list.size() != pre.length){
+			list.add(n);
+			for(int i = 0; i < pre.length; i++){
+				if(!list.contains(i)){
+					if(this.edges[n][i] != Integer.MAX_VALUE){
+						int tempPath = path[n] + edges[n][i];
+						if(tempPath < path[i]){
+							path[i] = tempPath;
+							pre[i] = n;
+						}
+					}
+				}
+			}
+			int min = Integer.MAX_VALUE, loc = -1;
+			for(int j = 0; j < path.length; j++){
+				if(!list.contains(j)){
+					if(path[j] < min){
+						min = path[j];
+						loc = j;
+					}
+				}
+			}
+			if(loc == -1){
+				break;
+			}else{
+				n = loc;
+			}
+			System.out.println(Arrays.toString(pre));
+			System.out.println(Arrays.toString(path));
+			System.out.println("------");
+		}
+		System.out.println(Arrays.toString(pre));
+		System.out.println(Arrays.toString(path));
+	}
+	
+	
+	public void floyd(){
+		System.out.println("floyd");
+		int[][] v = new int[this.vertexList.size()][this.vertexList.size()];
+		String[][] s = new String[this.vertexList.size()][this.vertexList.size()];
+		for(int i = 0; i < this.vertexList.size(); i++){
+			for(int j = 0; j < this.vertexList.size(); j++){
+				v[i][j] = this.edges[i][j];
+				s[i][j] = this.getValueByIndex(i);
+			}
+			v[i][i] = 0;
+		}
+		System.out.println("---------------");
+		this.showGraph(s);
+		System.out.println("---------------");
+		this.showGraph(v);
+		System.out.println("---------------");
+		for(int i = 0; i < v.length; i++){
+			
+			for(int j = 0; j < v.length; j++){
+				
+				for(int k = 0; k < v.length; k++){
+					int t = Integer.MAX_VALUE;
+					if(v[j][i] != Integer.MAX_VALUE && v[i][k] != Integer.MAX_VALUE){
+						t = v[j][i] + v[i][k];
+					}
+					if(v[j][k] > t){
+						v[j][k] = t;
+						s[j][k] = this.getValueByIndex(i);
+					}
+				}
+			}
+			System.out.println("round : " + i);
+			this.showGraph(s);
+			System.out.println("---------------");
+			this.showGraph(v);
+			System.out.println("---------------");
+		}
+		
+		System.out.println("---------------");
+		this.showGraph(s);
+		System.out.println("---------------");
+		this.showGraph(v);
+		System.out.println("---------------");
+		
+	}
+	
+	
+	
 	
 }
--- src/tenAlgorithm/Test1.java
+++ src/tenAlgorithm/Test1.java
@@ -1,5 +1,6 @@
 package tenAlgorithm;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,7 +8,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		knight();
 	}
 	
 	//算法一：二分查找
@@ -43,10 +44,205 @@ public class Test1 {
 	 * 
 	 */
 	public static void packagePro(){
-		int packageNum = 4;
+		int p = 4;
+		int n = 3;
+		int[] v = {1500,3000,2000};
+		int[] w = {1,4,3};
+		int matrix[][] = new int[n + 1][p + 1];
+		int path[][] = new int[n + 1][p + 1];
+		for(int i = 0; i < n + 1; i++){
+			matrix[i][0] = 0;
+		}
+		for(int i = 0; i < p + 1; i++){
+			matrix[0][p] = 0;
+		}
+		for(int i = 1; i < n + 1; i++){
+			for(int j = 1; j < p + 1; j++){
+				if(w[i - 1] > j){
+					matrix[i][j] = matrix[i - 1][j];
+				}else if(w[i - 1] <= j){
+					int temp = matrix[i - 1][j - w[i - 1]] + v[i - 1];
+					if(temp > matrix[i - 1][j])
+					{
+						matrix[i][j] = temp;
+						path[i][j] = 1;
+					}else{
+						matrix[i][j] = matrix[i - 1][j];
+					}
+				}
+			}
+			System.out.println(Arrays.toString(matrix[i]));
+		}
+		int i = path.length - 1;
+		int j = path[0].length - 1;
+		while(i > 0 && j > 0){
+			if(path[i][j] == 1){
+				System.out.println(i);
+				j = j - w[i - 1];
+			}
+			i--;
+		}
 		
 	}
 	
 	
+	public static void stringCmp(){
+		
+		String s1 = "123";
+		String s2 = "456123";
+		int i = 0, j = 0;
+		while(i < s1.length() && j < s2.length()){
+			if(s1.charAt(i) == s2.charAt(j)){
+				i++;
+				j++;
+			}else{
+				j = j - (i - 1);
+				i = 0;
+			}
+		}
+		System.out.println(i == s1.length());
+		System.out.println(i);
+		
+	}
+	
+	public static void kmp(){
+		String s1 = "bbcabcdababcdabcdabde";
+		String s2 = "abcdabd";
+		
+		// a b c d a b d
+		// 0 0 0 0 1 2 0
+		
+		/*
+		另一种找发：
+		next[0] = 0;
+		for(int i = 1, j = 0; i < s2.length(); i++){
+			while(j > 0&& s2.charAt(j) != s2.charAt(i)){
+				j = next[j - 1];
+			}
+			if(s2.charAt(i) == s2.charAt(j)){
+				j++;
+			}
+			next[i] = j;
+		}
+		
+		*/
+		
+		int[] next = new int[s2.length()];
+		Arrays.fill(next, 0);
+		for(int i = 1; i < next.length; i++){
+			if(s2.charAt(next[i - 1]) == s2.charAt(i)){//前缀和后缀的末字符相同
+				next[i] = next[i - 1] + 1;//匹配的后缀个数加一，同时值表示待匹配的下一个位置
+			}else{
+				int j = next[i - 1];//此时保证两个指针的前一个一定是匹配的
+				while(j > 0){
+					if(s2.charAt(next[j]) != s2.charAt(i)){
+						j = next[j];
+					}else{
+						next[i] = next[j] + 1;
+						break;
+					}
+				}
+				if(j == 0)
+					next[i] = 0;
+			}
+		}
+		System.out.println(Arrays.toString(next));
+		int n1 = 0, n2 = 0;
+		while(n1 < s1.length() && n2 < s2.length()){
+			if(s1.charAt(n1) == s2.charAt(n2)){
+				n1++;
+				n2++;
+			}else{
+				if(n2 != 0){
+					n1 = n1 - next[n2];
+					n2 = next[n2 - 1];
+				}else{
+					n1++;
+					n2 = 0;
+				}
+				
+			}
+		}
+		
+		System.out.println(n1 - n2);
+		
+	}
+	
+	
+	//骑士周游列国问题
+	
+	private static int X;//行数
+	private static int Y;//列数
+	
+	//根据当前位置计算还能走哪些位置，并将它们放入一个集合中，判断八个位置
+	public static ArrayList<Point> next(Point curPoint){
+		ArrayList<Point> ps = new ArrayList<>();
+		Point p1 = new Point();
+		if((p1.x = curPoint.x + 1) <= X && (p1.y = curPoint.y + 2) <= Y){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x + 2) <= X && (p1.y = curPoint.y + 1) <= Y){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x + 2) <= X && (p1.y = curPoint.y - 1) >= 0){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x + 1) <= X && (p1.y = curPoint.y - 2) >= 0){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y - 2) >= 0){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y - 1) >= 0){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y + 1) <= Y){
+			ps.add(new Point(p1));
+		}
+		if((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y + 2) <= Y){
+			ps.add(new Point(p1));
+		}
+		return ps;
+	}
+	
+	public static void sort(ArrayList<Point> ps){
+		ps.sort((p1, p2)->{
+			int count1 = next(p1).size();
+			int count2 = next(p2).size();
+			return Integer.compare(count1, count2);
+		});
+	}
+	
+	//判断棋盘各个位置是否被访问过
+	private static boolean visited[];
+	//标记是否期盼的所有位置都被访问
+	private static boolean finished;
+	
+	public static void knight(){
+		
+	}
+	
+	//step表示马当前走第几步，初始位置为1
+	public static void chess(int[][] chessboard, int row, int column, int step){
+		chessboard[row][column] = step;
+		visited[row * X + column] = true;
+		ArrayList<Point> ps = next(new Point(row, column));
+		sort(ps);//贪心算法优化
+		while(!ps.isEmpty()){
+			Point p = ps.remove(0);
+			if(visited[p.x * X + p.y]){
+				chess(chessboard, p.x, p.y, step + 1);
+			}
+		}
+		//判断是否完成任务，否则回溯
+		if(step < X * Y && !finished){
+			chessboard[row][column] = 0;
+			visited[row * X + column] = false;
+		}else{//
+			finished = true;
+		}
+	}
 
 }
+
+
